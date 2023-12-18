649ddeb44cbba085c69ce4fb1a9e5e1955f5a48d
yuriif671 <yuriif767@deltalearns.ca>
2023-12-18 12:48:38 -0800

    Done

--- problem1.py
+++ problem1.py
@@ -12,3 +12,59 @@ The program should display the factored form in an Entry widget.
 Extension: make the + between a,b and b,c buttons that will toggle
 between + and -.
 """
+
+import tkinter as tk
+from tkinter import ttk
+import math
+import cmath
+
+window = tk.Tk()
+
+window.geometry("520x300")
+window.title("Factoring ax^2 + bx + c = 0")
+
+def enter():
+    a = float(aEntry.get())
+    b = float(bEntry.get())
+    c = float(cEntry.get())
+
+    discriminant = (b ** 2) - (4 * a * c)
+
+    # Calculate the solutions
+    solution1 = (-b + cmath.sqrt(discriminant)) / (2 * a)
+    solution2 = (-b - cmath.sqrt(discriminant)) / (2 * a)
+
+    outputLabel.configure(text = f"Solutions are: \n{solution1}\n{solution2}.")
+
+#labels
+aLabel = tk.Label(window, text = "a")
+bLabel = tk.Label(window, text = "b")
+cLabel = tk.Label(window, text = "c")
+
+outputLabel = tk.Label(window)
+
+#entries
+aEntry = tk.Entry(window)
+
+bEntry = tk.Entry(window)
+cEntry = tk.Entry(window)
+
+#button
+enterButton = tk.Button(window, text = "Enter", command = enter)
+
+#grid
+aLabel.grid(row = 1, column = 1)
+bLabel.grid(row = 1, column = 2)
+cLabel.grid(row = 1, column = 3)
+
+aEntry.grid(row = 2, column = 1)
+bEntry.grid(row = 2, column = 2)
+cEntry.grid(row = 2, column = 3)
+
+aEntry.insert(0, 1)
+
+enterButton.grid(row = 3, column = 2)
+
+outputLabel.grid(row = 4, column = 2)
+
+window.mainloop()
